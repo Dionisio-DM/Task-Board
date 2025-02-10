2ca771a8edf80a9abef1e291c6f8ccd3037760ee
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-10 17:34:07 -0300

    feat: envolvendo app com provedor de contexto criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { Box, Flex, Heading } from "@radix-ui/themes";
 import { CreateTaskForm } from "./Components/CreateTaskForm";
 import { TaskBoard } from "./Components/TaskBoard";
+import { TasksContextProvider } from "./Context/TasksContext";
 
 function App() {
   return (
-    <Box maxWidth="80rem" mx="auto">
-      <Box height="4rem">
-        <Flex align="center" gap="4" height="100%">
-          <Heading as="h1" size="8" weight="light">
-            React Kanban
+    <TasksContextProvider>
+      <Box maxWidth="80rem" mx="auto">
+        <Box height="4rem">
+          <Flex align="center" gap="4" height="100%">
+            <Heading as="h1" size="8" weight="light">
+              React Kanban
+            </Heading>
+            <CreateTaskForm />
+          </Flex>
+        </Box>
+        <Box>
+          <Heading as="h2" mb={"4"}>
+            Quadro de tarefas
           </Heading>
-          <CreateTaskForm />
-        </Flex>
+          <TaskBoard></TaskBoard>
+        </Box>
       </Box>
-      <Box>
-        <Heading as="h2" mb={"4"}>
-          Quadro de tarefas
-        </Heading>
-        <TaskBoard></TaskBoard>
-      </Box>
-    </Box>
+    </TasksContextProvider>
   );
 }
 
